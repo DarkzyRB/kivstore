c4121e842f181e32d645b09010d36a2802be8233
Paul RB <paulsalcedo.dev@gmail.com>
2025-06-22 01:49:17 -0500

    Fix flow-support

--- flow-support/src/main/java/com/kivpson/extensions/kivstore/flow_support/KivStoreFlowExtensions.kt
+++ flow-support/src/main/java/com/kivpson/extensions/kivstore/flow_support/KivStoreFlowExtensions.kt
@@ -2,7 +2,9 @@ package com.kivpson.extensions.kivstore.flow_support
 
 import com.kivpson.extensions.kivstore.KivStoreModel
 import com.kivpson.extensions.kivstore.types.AbstractDataStoreType
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
@@ -12,18 +14,18 @@ import kotlin.reflect.jvm.isAccessible
 fun <T : Any> KivStoreModel.asFlow(property: KMutableProperty0<T>): Flow<T> = flow {
     property.isAccessible = true
 
-    val delegate = property.getDelegate()
-    require(delegate is AbstractDataStoreType<*>) {
-        "Property ${property.name} is not backed by a KivStore type"
+    val delegate = property.getDelegate().let {
+        require(it is AbstractDataStoreType<*>) {
+            "Property ${property.name} is not backed by a KivStore type"
+        }
+        @Suppress("UNCHECKED_CAST")
+        it as AbstractDataStoreType<T>
     }
 
-    @Suppress("UNCHECKED_CAST")
-    val typedDelegate = delegate as AbstractDataStoreType<T>
-
-    emit(typedDelegate.readValue())
+    emit(delegate.readValue())
 
     dataStore.data
-        .map { typedDelegate.readValue() }
+        .map { delegate.readValue() }
         .distinctUntilChanged()
         .collect { emit(it) }
-}
+}.buffer(Channel.BUFFERED)
